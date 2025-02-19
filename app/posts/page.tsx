3e8ff08585c05f30b6747e3c57bd07eed2ca679f
Normandia Akbar <normnd.akbr2@gmail.com>
2025-02-19 18:09:08 +0800

    added fetch data from jsontypicode

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -3,10 +3,23 @@ import "../components/CardList";
 import CardList from "../components/CardList";
 // import ViewUserButton from "../components/ViewUserButton";
 
-const Posts = () => {
+const base_url = "https://jsonplaceholder.typicode.com/posts"
+
+interface PostsData {
+    userId: number,
+    id: number,
+    title: string,
+    body: string
+}
+
+const Posts = async () => {
+    const response = await fetch(base_url)
+    const posts: PostsData[] = await response.json();
+
     return (
         <>
             <h1 className="text-fuchsia-500">POST PAGE</h1>
+            <p>{posts[0].id}</p>
             <CardList>
                 <p>Hello</p>
                 <i>Hi!</i>
